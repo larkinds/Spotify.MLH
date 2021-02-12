25ab83105563d8ff4a5c8dbf3a5a2e71fe26996d
Larkin <larkindsmith@gmail.com>
2021-02-11 21:58:59 -0500

    scaffolded app

---
+++ src/extension.ts
@@ -0,0 +1,25 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "spotifymlh" is now active!');
+
+
+	context.subscriptions.push(vscode.commands.registerCommand("spotifymlh.play", () => {
+		//logic for play on spotify goes here
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand("spotifymlh.pause", () => {
+		//logic for pause on spotify goes here
+	}));
+
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
