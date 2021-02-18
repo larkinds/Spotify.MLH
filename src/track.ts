ad9d7f59a42ce9c2b836db1bdc12387ee420dc1b
James Martindale <11380394+jkmartindale@users.noreply.github.com>
2021-02-18 16:36:52 -0600

    Revert "Revert "View Actions for History panel""
    This reverts commit 6983f46cb499021bb7c13ee4f20fade4c5f14357.

--- src/track.ts
+++ src/track.ts
@@ -8,12 +8,16 @@ import * as vscode from 'vscode';
 export default class Track extends vscode.TreeItem {
     artists: any[];
     name: string;
+    uri: string;
+    
     constructor(
-        private readonly track: any
+        public readonly data: any
     ) {
-        super(track.name);
-        this.name = track.name;
-        this.artists = track.artists;
+        super(data.name);
+        this.contextValue = 'track';
+        this.name = data.name;
+        this.artists = data.artists;
+        this.uri = data.uri;
 
         this.description = this.artists.map(x => x.name).join(', ');
         this.tooltip = new vscode.MarkdownString(`**${this.name}**  \n${this.artists.map(x => x.name).join(', ')}`);
